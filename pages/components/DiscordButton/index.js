29fc0ef72ca4f83c0e190e51ac3718d057570499
CiaraMaria <proiacm@gmail.com>
2022-02-28 08:11:13 -0700

    adjust styling

--- pages/components/DiscordButton/index.js
+++ pages/components/DiscordButton/index.js
@@ -3,10 +3,15 @@ import Image from "next/image";
 export default function DiscordButton() {
 
   return (
-    <button onClick={()=> window.open("https://discord.gg/sgU3txDcsV", "_blank", "noopener noreferrer")} className="flex mt-6">
-      <div className="flex items-center w-[28rem] px-6 py-1 leading-none rounded-full bg-[#14B8A6] text-white">
+    <button
+      onClick={()=>
+        window.open("https://discord.gg/sgU3txDcsV", "_blank", "noopener noreferrer")
+      }
+      className="flex mt-6"
+    >
+      <div className="flex items-center px-6 py-1 leading-none rounded-full bg-[#14B8A6] text-white">
         <span className="leading-7 tracking-wide text-md">Join the Discord</span>
-        <span className="ml-64"><Image src="/../public/arrow.png" alt="arrow" height="14px" width="14px"/></span>
+        <span className="ml-6 lg:ml-64"><Image src="/../public/arrow.png" alt="arrow" height="14px" width="14px"/></span>
       </div>
     </button>
   )
