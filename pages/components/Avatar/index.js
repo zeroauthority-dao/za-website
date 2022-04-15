379af1dde5fbfe549c556a632d45999ff9e33440
kenny <krogers@hey.com>
2022-04-15 00:59:53 -0600

    Add avatar component

---
+++ pages/components/Avatar/index.js
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../../../utils/supabaseClient";
+
+export default function Avatar({ url, size, onUpload }) {
+  const [avatarUrl, setAvatarUrl] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
+  useEffect(() => {
+    if (url) downloadImage(url);
+  }, [url]);
+
+  async function downloadImage(path) {
+    try {
+      const { data, error } = await supabase.storage
+        .from("avatars")
+        .download(path);
+      if (error) {
+        throw error;
+      }
+      const url = URL.createObjectURL(data);
+      setAvatarUrl(url);
+    } catch (error) {
+      console.log("Error downloading image: ", error.message);
+    }
+  }
+
+  async function uploadAvatar(event) {
+    try {
+      setUploading(true);
+
+      if (!event.target.files || event.target.files.length === 0) {
+        throw new Error("You must select an image to upload.");
+      }
+
+      const file = event.target.files[0];
+      const fileExt = file.name.split(".").pop();
+      const fileName = `${Math.random()}.${fileExt}`;
+      const filePath = `${fileName}`;
+
+      let { error: uploadError } = await supabase.storage
+        .from("avatars")
+        .upload(filePath, file);
+
+      if (uploadError) {
+        throw uploadError;
+      }
+
+      onUpload(filePath);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setUploading(false);
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      {avatarUrl ? (
+        <img
+          src={avatarUrl}
+          alt="Avatar"
+          className="avatar image rounded-lg"
+          style={{ height: size, width: size }}
+        />
+      ) : (
+        <div
+          className="bg-gray-400 rounded-lg"
+          style={{ height: size, width: size }}
+        />
+      )}
+      <div style={{ width: size }}>
+        <label
+          className="bg-primary text-white px-4 py-2 rounded-lg shadow hover:cursor-pointer"
+          htmlFor="single"
+        >
+          {uploading ? "Uploading ..." : "Upload Avatar"}
+        </label>
+        <input
+          style={{
+            visibility: "hidden",
+            position: "absolute",
+          }}
+          type="file"
+          id="single"
+          accept="image/*"
+          onChange={uploadAvatar}
+          disabled={uploading}
+        />
+      </div>
+    </div>
+  );
+}
