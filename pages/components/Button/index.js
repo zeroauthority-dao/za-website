276399ab0b05ce36951c23a3e14eeeef6fdd76be
kenny <krogers@hey.com>
2022-07-07 09:36:56 -0600

    Fix button properties

--- pages/components/Button/index.js
+++ pages/components/Button/index.js
@@ -1,32 +1,28 @@
 import Image from "next/image";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
-const Button = ({
-  buttonData
-}) => {
-  const router = useRouter()
+const Button = ({ text, path }) => {
+  const router = useRouter();
 
   return (
     <button
-      onClick={()=>
-        router.push(`${buttonData.path}`)
-      }
+      onClick={() => router.push(`${path}`)}
       className="mt-6 w-full max-w-content"
     >
       <div className="grid md:grid-cols-2 px-6 py-2 rounded-full bg-[#14B8A6] text-white">
         <div className="grid md:place-items-start">
           <div className="text-sm whitespace-nowrap md:text-lg font-semibold">
-            {buttonData.text}
+            {text}
           </div>
         </div>
         <div className="hidden md:grid place-items-end">
           <div>
-            <Image src="/arrow.png" alt="arrow" height="14px" width="14px"/>
+            <Image src="/arrow.png" alt="arrow" height="14px" width="14px" />
           </div>
         </div>
       </div>
     </button>
-  )
-}
+  );
+};
 
 export default Button;
