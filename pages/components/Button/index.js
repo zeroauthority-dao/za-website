23c0f59258fb0c10c604e979280415c857ca847b
kenny <krogers@hey.com>
2022-07-11 11:48:06 -0600

    Push button data fix

--- pages/components/Button/index.js
+++ pages/components/Button/index.js
@@ -1,18 +1,18 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
 
-const Button = ({ text, path }) => {
+const Button = ({ buttonData }) => {
   const router = useRouter();
 
   return (
     <button
-      onClick={() => router.push(`${path}`)}
+      onClick={() => router.push(`${buttonData.path}`)}
       className="mt-6 w-full max-w-content"
     >
       <div className="grid md:grid-cols-2 px-6 py-2 rounded-full bg-[#14B8A6] text-white">
         <div className="grid md:place-items-start">
           <div className="text-sm whitespace-nowrap md:text-lg font-semibold">
-            {text}
+            {buttonData.text}
           </div>
         </div>
         <div className="hidden md:grid place-items-end">
